049a9bd5564bf80b72ef318ce368e275a4492756
Alex_Koss <cahek27@mail.ru>
2025-11-06 00:15:02 +0300

    init project

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['public/css/main.css', 'resources/src/index.jsx'],
+            refresh: true,
+        }),
+        react(),
+    ],
+});
