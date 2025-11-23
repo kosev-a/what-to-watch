a6ca41a825f664e51f7740bc39016d7fa427025f
Alex_Koss <cahek27@mail.ru>
2025-11-23 15:52:09 +0300

    add login/logout on frontend

--- resources/src/components/app/app.jsx
+++ resources/src/components/app/app.jsx
@@ -1,79 +1,119 @@
-import React, { Fragment } from 'react';
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
-import {AppRoute} from '../../const';
-import PropTypes from 'prop-types';
-import Main from '../pages/main/main';
-import SignIn from '../pages/signin/signin';
-import SignUp from '../pages/signup/signup';
-import MyList from '../pages/mylist/mylist';
-import Film from '../pages/film/film';
-import Review from '../ui/review/review';
-import Player from '../pages/player/player';
-import filmProp from '../ui/card/card.prop';
-import reviewProp from '../ui/review/review.prop';
-import {getFilm, getReviews} from '../../utils/utils';
-
+import React, { Fragment, useState, useEffect } from "react";
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+import { AppRoute } from "../../const";
+import PropTypes from "prop-types";
+import Main from "../pages/main/main";
+import SignIn from "../pages/signin/signin";
+import SignUp from "../pages/signup/signup";
+import MyList from "../pages/mylist/mylist";
+import Film from "../pages/film/film";
+import Review from "../ui/review/review";
+import Player from "../pages/player/player";
+import filmProp from "../ui/card/card.prop";
+import reviewProp from "../ui/review/review.prop";
+import { getFilm, getReviews } from "../../utils/utils";
 
 function App(props) {
-  const {films,reviews, name, genre, year} = props;
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" exact >
-          <Main films={films} name={name} genre={genre} year={year} />
-        </Route>
-        <Route path="/login" exact component={SignIn} />
-        <Route path="/signup" exact component={SignUp} />
-        <Route path="/mylist" exact >
-          <MyList films={films} />
-        </Route>
-        <Route
-          exact path={`${AppRoute.FILM}/:id`}
-          render={(data) => (
-            <Film
-              film={getFilm(films, data.match.params.id)}
-              films={films}
-              reviews={reviews}
-            />)}
-        />
-        <Route
-          exact path={`${AppRoute.FILM}/:id/review`}
-          render={(data) => (
-            <Review
-              review={getReviews(reviews, data.match.params.id)}
-            />)}
-        />
-        <Route
-          exact path={`${AppRoute.PLAYER}/:id`}
-          render={(data) => (
-            <Player
-              film={getFilm(films, data.match.params.id)}
-            />
-          )}
-        />
-        <Route
-          render={() => (
-            <Fragment>
-              <h1>
-                404.
-                <br />
-                <small>Page not found</small>
-              </h1>
-              <Link to="/">Go to main page</Link>
-            </Fragment>
-          )}
-        />
-      </Switch>
-    </BrowserRouter>
-  );
+    const { films, reviews, name, genre, year } = props;
+    const [user, setUser] = useState(localStorage.getItem("user")) || null;
+    console.log(user);
+    const apiUrl = import.meta.env.VITE_APP_URL;
+
+    const handleLogout = async (e) => {
+        e.preventDefault(); // Предотвращаем стандартное поведение формы, хотя формы здесь нет, это хорошая практика для обработчиков
+
+        try {
+            const response = await fetch(`${apiUrl}/api/logout`, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+            });
+
+            if (response.ok) {
+                // Сервер подтвердил выход, теперь очищаем локальные данные
+                setUser(null);
+                localStorage.removeItem("user");
+                localStorage.removeItem("token");
+                // Перенаправляем пользователя
+                // window.location.href = '/login';
+            } else {
+                // Обрабатываем ошибки сервера
+                console.error("Ошибка при выходе из системы на сервере");
+            }
+        } catch (error) {
+            console.error("Ошибка сети:", error);
+        }
+    };
+
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact>
+                    <Main
+                        films={films}
+                        name={name}
+                        genre={genre}
+                        year={year}
+                        user={user}
+                        onLogout={handleLogout}
+                    />
+                </Route>
+                <Route path="/login" exact>
+                    <SignIn />
+                </Route>
+                <Route path="/signup" exact component={SignUp} />
+                <Route path="/mylist" exact>
+                    <MyList films={films} />
+                </Route>
+                <Route
+                    exact
+                    path={`${AppRoute.FILM}/:id`}
+                    render={(data) => (
+                        <Film
+                            film={getFilm(films, data.match.params.id)}
+                            films={films}
+                            reviews={reviews}
+                        />
+                    )}
+                />
+                <Route
+                    exact
+                    path={`${AppRoute.FILM}/:id/review`}
+                    render={(data) => (
+                        <Review
+                            review={getReviews(reviews, data.match.params.id)}
+                        />
+                    )}
+                />
+                <Route
+                    exact
+                    path={`${AppRoute.PLAYER}/:id`}
+                    render={(data) => (
+                        <Player film={getFilm(films, data.match.params.id)} />
+                    )}
+                />
+                <Route
+                    render={() => (
+                        <Fragment>
+                            <h1>
+                                404.
+                                <br />
+                                <small>Page not found</small>
+                            </h1>
+                            <Link to="/">Go to main page</Link>
+                        </Fragment>
+                    )}
+                />
+            </Switch>
+        </BrowserRouter>
+    );
 }
 
 App.propTypes = {
-  films: PropTypes.arrayOf(filmProp).isRequired,
-  reviews: PropTypes.arrayOf(reviewProp).isRequired,
-  name: PropTypes.string.isRequired,
-  genre: PropTypes.string.isRequired,
-  year: PropTypes.number.isRequired,
+    films: PropTypes.arrayOf(filmProp).isRequired,
+    reviews: PropTypes.arrayOf(reviewProp).isRequired,
+    name: PropTypes.string.isRequired,
+    genre: PropTypes.string.isRequired,
+    year: PropTypes.number.isRequired,
 };
 
 export default App;
